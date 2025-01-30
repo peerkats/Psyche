8effdb60f4751417b6e950477c434318432f7f69
peerkats <peerkats@gmail.com>
2025-01-30 21:03:39 +0100

    initial commit

---
+++ psyche/target/debug/deps/psyche-2ebfca9c0956267b.d
@@ -0,0 +1,5 @@
+/Users/peerkats/psyche/psyche/target/debug/deps/psyche-2ebfca9c0956267b: src/main.rs
+
+/Users/peerkats/psyche/psyche/target/debug/deps/psyche-2ebfca9c0956267b.d: src/main.rs
+
+src/main.rs:
---
+++ psyche/target/debug/psyche.d
@@ -0,0 +1,1 @@
+/Users/peerkats/psyche/psyche/target/debug/psyche: /Users/peerkats/psyche/psyche/src/main.rs
